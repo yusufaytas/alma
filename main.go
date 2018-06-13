72e07bee83023379ac36d9480c3132f37b747dd8
Yusuf Aytas <yusufaytas@gmail.com>
2018-06-13 01:43:53 +0100

    Added the initial ping.

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-    println("hello!")
+	ping("http://www.yusufaytas.com")
+}
+
+func ping(domain string) {
+	response, _ := http.Get(domain)
+	fmt.Printf(response.Status)
 }
